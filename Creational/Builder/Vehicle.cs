811b8d8f4d41d1564e0e79996624634a7ef498ab
Borja Garcia <95009610@lidl.de>
2018-10-28 21:18:56 +0100

    builder definition

--- Creational/Builder/Vehicle.cs
+++ Creational/Builder/Vehicle.cs
@@ -1,4 +1,4 @@
-﻿namespace Creational
+﻿namespace Creational.Builder
 {
     public class Vehicle
     {
--- Creational/Builder/VehicleBuilder.cs
+++ Creational/Builder/VehicleBuilder.cs
@@ -1,5 +1,9 @@
 ﻿namespace Creational.Builder
 {
+    /// <summary>
+    /// Separate the construction of a complex object from its representation
+    /// so that the same construction process can create different representations.
+    /// </summary>
     public class VehicleBuilder
     {
         private static int Wheels { get; set; }
