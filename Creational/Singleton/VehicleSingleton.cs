32813ebd776aa9db539bc7cc795745aed628ba60
Borja Garcia <95009610@lidl.de>
2018-11-02 20:28:47 +0100

    singleton pattern

---
+++ Creational/Singleton/VehicleSingleton.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Creational.Singleton
+{
+    /// <summary>
+    /// Ensure a class has only one instance
+    /// and provide a global point of access to it.
+    /// </summary>
+    public class VehicleSingleton
+    {
+        private VehicleSingleton() { }
+
+        public static VehicleSingleton Instance { get; } = new VehicleSingleton();
+
+        public string Name { get; set; }
+    }
+}
---
+++ DesignPatternsTest/Creational/Singleton/VehicleSingletonTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Creational.Singleton;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DesignPatternsTest.Creational.Singleton
+{
+    [TestClass]
+    public class VehicleSingletonTest
+    {
+        [TestMethod]
+        public void Given_AVehicle_When_ChangeName_Should_BeTheSame()
+        {
+            var firstVehicle = VehicleSingleton.Instance;
+            var secondVehicle = VehicleSingleton.Instance;
+
+            firstVehicle.Name = "volkswagen t5";
+
+            Assert.AreEqual(firstVehicle.Name, secondVehicle.Name);
+        }
+    }
+}
