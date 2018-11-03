a2798c1c9d2b1fea53353b79656f2428a0c61a91
Borja Garcia <95009610@lidl.de>
2018-11-03 13:23:12 +0100

    strategy pattern

---
+++ Behavioral/Strategy/NotificationSender.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Behavioral.Strategy
+{
+    public interface INotificationSender
+    {
+        string SendNotification(string target, string content);
+    }
+
+    public interface INotificationProvider
+    {
+        string Send(string target, string content);
+    }
+
+    public class NotificationSender : INotificationSender
+    {
+        private INotificationProvider _provider;
+
+        public NotificationSender(INotificationProvider provider)
+        {
+            _provider = provider;
+        }
+
+        public string SendNotification(string target, string content)
+        {
+            return _provider.Send(target, content);
+        }
+
+        public void ChangeProvider(INotificationProvider provider)
+        {
+            _provider = provider;
+        }
+    }
+
+    public class MobileAppNotificationSender : NotificationSender
+    {
+        public MobileAppNotificationSender()
+            : base(new SmsProviderOne())
+        {
+
+        }
+    }
+
+    public class SmsProviderOne : INotificationProvider
+    {
+        public string Send(string target, string content)
+        {
+            throw new Exception("No signal");
+        }
+    }
+
+    public class SmsProviderTwo : INotificationProvider
+    {
+        public string Send(string target, string content)
+        {
+            return $"Sms sended to:{target}";
+        }
+    }
+
+}
---
+++ DesignPatternsTest/Behavioral/Strategy/StrategyPatternTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Behavioral.Strategy;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DesignPatternsTest.Behavioral.Strategy
+{
+    [TestClass]
+    public class StrategyPatternTest
+    {
+        [TestMethod]
+        [DataRow("697111888", "welcome to strategy pattern")]
+        public void Given_AnApp_When_SendNotification_Should_Send(string target, string content)
+        {
+            var smsNotificationSender = new MobileAppNotificationSender();
+
+            try
+            {
+                smsNotificationSender.SendNotification(target, content);
+            }
+            catch (Exception)
+            {
+                //Provider 1 fail, switch to provider 2
+                smsNotificationSender.ChangeProvider(new SmsProviderTwo());
+            }
+
+            smsNotificationSender.SendNotification(target, content);
+        }
+    }
+}
