481dea8c41aaaaf202f79224ea961d5691f946a2
Borja Garcia <95009610@lidl.de>
2018-11-10 17:38:31 +0100

    chain of responsability

---
+++ Behavioral/ChainOfResponsability/SmsValidationRule.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Behavioral.ChainOfResponsability
+{
+    public abstract class SmsValidationRule
+    {
+        protected SmsValidationRule NextRule;
+
+        public void SetNext(SmsValidationRule rule)
+        {
+            NextRule = rule;
+        }
+
+        public abstract bool IsValid(SmsValidation smsValidation);
+    }
+
+    public class AttemptsRule : SmsValidationRule
+    {
+        private const int MaxAttempts = 5;
+
+        public override bool IsValid(SmsValidation smsValidation)
+        {
+            if (smsValidation.Attempts >= MaxAttempts)
+            {
+                return false;
+            }
+
+            return NextRule?.IsValid(smsValidation) ?? true;
+        }
+    }
+
+    public class ValidCodeRule : SmsValidationRule
+    {
+        public override bool IsValid(SmsValidation smsValidation)
+        {
+            if (smsValidation.UserInput != smsValidation.Code)
+            {
+                return false;
+            }
+
+            return NextRule?.IsValid(smsValidation) ?? true;
+        }
+    }
+
+    public class ExpiredRule : SmsValidationRule
+    {
+        private const int ExpirationMinutes = 30;
+
+        public override bool IsValid(SmsValidation smsValidation)
+        {
+            if (smsValidation.Sent.AddMinutes(ExpirationMinutes) > DateTime.UtcNow)
+            {
+                return false;
+            }
+
+            return NextRule?.IsValid(smsValidation) ?? true;
+        }
+    }
+
+    public class SmsValidation
+    {
+        public string Code { get; set; }
+        public int Attempts { get; set; }
+        public string UserInput { get; set; }
+        public DateTime Sent { get; set; }
+    }
+}
---
+++ DesignPatternsTest/Behavioral/ChainOfResponsability/ChainOfResponsabilityTest.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Behavioral.ChainOfResponsability;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DesignPatternsTest.Behavioral.ChainOfResponsability
+{
+    [TestClass]
+    public class ChainOfResponsabilityTest
+    {
+        [TestMethod]
+        public void Given_ASmsValidation_When_MaxAttemps_Should_BeNotValid()
+        {
+            var smsValidation = new SmsValidation
+            {
+                Code = "5500",
+                Attempts = 6,
+                UserInput = "5501",
+                Sent = DateTime.UtcNow.AddMinutes(-10)
+            };
+
+            SmsValidationRule attemptsRule = new AttemptsRule();
+            attemptsRule.SetNext(new ExpiredRule());
+            attemptsRule.SetNext(new ValidCodeRule());
+
+            Assert.IsFalse(attemptsRule.IsValid(smsValidation));
+        }
+
+        [TestMethod]
+        public void Given_ASmsValidation_When_InvalidCode_Should_BeNotValid()
+        {
+            var smsValidation = new SmsValidation
+            {
+                Code = "5500",
+                Attempts = 0,
+                UserInput = "5501",
+                Sent = DateTime.UtcNow.AddMinutes(-10)
+            };
+
+            SmsValidationRule attemptsRule = new AttemptsRule();
+            attemptsRule.SetNext(new ExpiredRule());
+            attemptsRule.SetNext(new ValidCodeRule());
+
+            Assert.IsFalse(attemptsRule.IsValid(smsValidation));
+        }
+
+        [TestMethod]
+        public void Given_ASmsValidation_When_Valid_Should_ReturnValid()
+        {
+            var smsValidation = new SmsValidation
+            {
+                Code = "5500",
+                Attempts = 0,
+                UserInput = "5500",
+                Sent = DateTime.UtcNow.AddMinutes(-10)
+            };
+
+            SmsValidationRule attemptsRule = new AttemptsRule();
+            attemptsRule.SetNext(new ExpiredRule());
+            attemptsRule.SetNext(new ValidCodeRule());
+
+            Assert.IsTrue(attemptsRule.IsValid(smsValidation));
+        }
+    }
+}
