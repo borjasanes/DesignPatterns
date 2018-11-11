6a275c52696d5ac28ed1214205f7b011cf959fdd
Borja Garcia <95009610@lidl.de>
2018-11-11 17:05:34 +0100

    observer pattern

---
+++ Behavioral/Observer/Contact.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml.Linq;
+
+namespace Behavioral.Observer
+{
+    public class Contact : IObservable<Contact>
+    {
+        private readonly List<IObserver<Contact>> _observers = new List<IObserver<Contact>>();
+
+        public IDisposable Subscribe(IObserver<Contact> observer)
+        {
+            if (!_observers.Contains(observer))
+            {
+                _observers.Add(observer);
+            }
+
+            return new UnsubscribeContact(_observers, observer);
+        }
+
+        private string _name { get; set; }
+
+        public string Name
+        {
+            get => _name;
+            set
+
+            {
+                if (_name == value) return;
+                _name = value;
+                Notify();
+            }
+        }
+
+        private void Notify()
+        {
+            foreach (var observer in _observers)
+            {
+                observer.OnNext(this);
+            }
+        }
+    }
+
+    public abstract class Observer<T>
+    {
+        public abstract void Update(T subject);
+    }
+
+    public class CrmObserver : IObserver<Contact>
+    {
+        public string Name { get; set; }
+
+        public void OnCompleted()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnError(Exception error)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnNext(Contact value)
+        {
+            Name = value.Name;
+        }
+    }
+
+    public class UnsubscribeContact : IDisposable
+    {
+        private readonly IList<IObserver<Contact>> _observers;
+        private readonly IObserver<Contact> _observer;
+
+        public UnsubscribeContact(IList<IObserver<Contact>> observers, IObserver<Contact> observer)
+        {
+            _observers = observers;
+            _observer = observer;
+        }
+
+        public void Dispose()
+        {
+            if (_observer != null) _observers.Remove(_observer);
+        }
+    }
+}
---
+++ DesignPatternsTest/Behavioral/Observer/ObserverTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Behavioral.Observer;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DesignPatternsTest.Behavioral.Observer
+{
+    [TestClass]
+    public class ObserverTest
+    {
+        [TestMethod]
+        public void Given_AContact_When_IsUpdated_Should_NotifyObservers()
+        {
+            var subject = new Contact();
+            var observer = new CrmObserver();
+
+            var unsubscribe = subject.Subscribe(observer);
+
+            subject.Name = "test";
+
+            Assert.AreEqual(subject.Name, observer.Name);
+
+            unsubscribe.Dispose();
+
+            subject.Name = "test2";
+
+            Assert.AreNotEqual(subject.Name, observer.Name);
+
+        }
+    }
+}
