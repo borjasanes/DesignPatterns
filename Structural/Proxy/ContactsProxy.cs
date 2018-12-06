c337aade9bd77e13bda4b91683ada926c3c2f017
Borja Garcia <95009610@lidl.de>
2018-12-06 13:30:55 +0100

    proxy

---
+++ Structural/Proxy/ContactsProxy.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace Structural.Proxy
+{
+    /// <summary>
+    /// Provide a placeholder for another object to control access to it.
+    /// </summary>
+    public class ContactsProxy
+    {
+        private readonly string _apiKey;
+        private static readonly HttpClient HttpClient = new HttpClient();
+
+        public ContactsProxy(string host, string apiKey)
+        {
+            _apiKey = apiKey;
+            HttpClient.BaseAddress = new Uri(host);
+        }
+
+        public async Task<string> GetContactName(string email)
+        {
+            HttpClient.DefaultRequestHeaders.Authorization = GetAuthenticationHeaderValue();
+
+            return await HttpClient.GetStringAsync($"/contact/{email}");
+        }
+
+        private AuthenticationHeaderValue GetAuthenticationHeaderValue()
+        {
+            return new AuthenticationHeaderValue(_apiKey);
+        }
+    }
+}
