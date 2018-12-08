f87700d2e966be2f1e6425613fba7c1828e51cca
Borja Garcia <95009610@lidl.de>
2018-12-08 11:43:17 +0100

    Decorator pattern

---
+++ DesignPatternsTest/Structural/StructuralTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Structural.Decorator;
+
+namespace DesignPatternsTest.Structural
+{
+    [TestClass]
+    public class StructuralTest
+    {
+        [TestMethod]
+        public void Given_ACoupon_When_IsDecorated_Should_AddFunctionality()
+        {
+            var sut = new Coupon { BaseDiscount = 10, Color = "Red", Title = "Welcome" };
+
+            Assert.AreEqual(10, sut.CalculateDiscount());
+
+            var sut2 = new CouponBio { BaseDiscount = 10, Color = "Red", Title = "Welcome Bio" };
+
+            Assert.AreEqual(1.5, sut2.CalculateDiscount());
+        }
+    }
+}
---
+++ Structural/Decorator/Coupon.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Structural.Decorator
+{
+    public class Coupon
+    {
+        public string Color { get; set; }
+        public long BaseDiscount { get; set; }
+        public string Title { get; set; }
+
+        public double CalculateDiscount()
+        {
+            return BaseDiscount;
+        }
+    }
+
+    public class CouponBio : Coupon
+    {
+        public new double CalculateDiscount()
+        {
+            return base.CalculateDiscount() * .15;
+        }
+    }
+
+    public class CouponVegan : Coupon
+    {
+        public new double CalculateDiscount()
+        {
+            return base.CalculateDiscount() * .20;
+        }
+    }
+}
